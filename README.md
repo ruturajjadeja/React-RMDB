95e671d028055793708c5ebada79cfd59dd22b1c
Ruturaj <ruturajjadeja21@gmail.com>
2019-08-28 12:37:47 -0500

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-# React-RMDB
-A light version of an IMDB style React DB Web Application,
-API by 'https://www.themoviedb.org'
---
+++ precache-manifest.f17875a749fe6f81b8af5cab454afedd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1867a73ff164897eb3ad29f17b8d586e",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "79c6f1758e88c906dcb6",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "5f44b6618c2d910f2e6c",
+    "url": "/robofriends/static/css/main.ee72f1c0.chunk.css"
+  },
+  {
+    "revision": "79c6f1758e88c906dcb6",
+    "url": "/robofriends/static/js/2.80c069d3.chunk.js"
+  },
+  {
+    "revision": "5f44b6618c2d910f2e6c",
+    "url": "/robofriends/static/js/main.9873957e.chunk.js"
+  },
+  {
+    "revision": "8bd552311f5d528a5605",
+    "url": "/robofriends/static/js/runtime~main.e3d5a5e7.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
