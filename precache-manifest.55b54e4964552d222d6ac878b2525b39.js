082cacd76e79d8bb85f5861f7ea902830ca867b1
Ruturaj <ruturajjadeja21@gmail.com>
2019-08-28 12:43:57 -0500

    Updates

---
+++ precache-manifest.55b54e4964552d222d6ac878b2525b39.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bb01bb5cc4fb40c2ce7b000576863bc",
+    "url": "/React-RMDB/index.html"
+  },
+  {
+    "revision": "37dc6e9b3b1a5916b641",
+    "url": "/React-RMDB/static/css/main.bb39a4fe.chunk.css"
+  },
+  {
+    "revision": "4e54180ac0c96d31ebd1",
+    "url": "/React-RMDB/static/js/2.c1af4691.chunk.js"
+  },
+  {
+    "revision": "37dc6e9b3b1a5916b641",
+    "url": "/React-RMDB/static/js/main.03dff63d.chunk.js"
+  },
+  {
+    "revision": "50080e0022697b89bdbc",
+    "url": "/React-RMDB/static/js/runtime~main.877db200.js"
+  }
+]);
